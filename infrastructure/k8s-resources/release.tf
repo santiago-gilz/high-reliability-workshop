b3b38f8d0e6b9b4e0b4c15687d93326f20a25a4b
santiago-gilz <santiago.gilz@perficient.com>
2022-05-05 05:18:59 -0500

    Added tf helm release for zipkin and log-msg-proc

--- infrastructure/k8s-resources/release.tf
+++ infrastructure/k8s-resources/release.tf
@@ -1,5 +1,13 @@
 locals {
-  redis_env_vars = {}
+  # ENV Variables
+  redis_env_vars  = {}
+  zipkin_env_vars = {}
+  log_msg_proc_env_vars = {
+    REDIS_HOST    = var.redis_long_name
+    REDIS_PORT    = var.redis_port
+    REDIS_CHANNEL = "log_channel"
+    ZIPKIN_URL    = "http://${var.zipkin_long_name}:${var.zipkin_port}/api/v2/spans"
+  }
 }
 
 resource "helm_release" "redis-queue" {
@@ -18,5 +26,42 @@ resource "helm_release" "redis-queue" {
         redis_short_name    = var.redis_short_name
         redis_long_name     = var.redis_long_name
         redis_port          = var.redis_port
+  })]
+}
+
+resource "helm_release" "zipkin" {
+  depends_on = [helm_release.redis-queue]
+  name       = var.zipkin_release_name
+  namespace  = var.namespace_name
+  chart      = "${path.module}/zipkin"
+  values = [
+    templatefile("${path.module}/zipkin/values.tpl.yaml",
+      {
+        zipkin_replicaCount   = var.zipkin_replicaCount
+        zipkin_restartPolicy  = var.zipkin_restartPolicy
+        zipkin_env_vars       = indent(2, yamlencode(local.zipkin_env_vars))
+        zipkin_registry       = var.zipkin_registry
+        zipkin_pull_policy    = var.zipkin_pull_policy
+        zipkin_short_name     = var.zipkin_short_name
+        zipkin_long_name      = var.zipkin_long_name
+        zipkin_port           = var.zipkin_port
+  })]
+}
+
+resource "helm_release" "log_msg_proc" {
+  depends_on = [helm_release.redis-queue]
+  name       = var.log_msg_proc_release_name
+  namespace  = var.namespace_name
+  chart      = "${path.module}/log-message-processor"
+  values = [
+    templatefile("${path.module}/log-message-processor/values.tpl.yaml",
+      {
+        log_msg_proc_replicaCount  = var.log_msg_proc_replicaCount
+        log_msg_proc_restartPolicy = var.log_msg_proc_restartPolicy
+        log_msg_proc_env_vars      = indent(2, yamlencode(local.log_msg_proc_env_vars))
+        log_msg_proc_registry      = var.log_msg_proc_registry
+        log_msg_proc_pull_policy   = var.log_msg_proc_pull_policy
+        log_msg_proc_short_name    = var.log_msg_proc_short_name
+        log_msg_proc_long_name     = var.log_msg_proc_long_name
   })]
 }
--- infrastructure/k8s-resources/variables.tf
+++ infrastructure/k8s-resources/variables.tf
@@ -17,15 +17,19 @@ variable "namespace_name" {
   description = "Your K8S namespace name"
 }
 
-variable "redis_release_name" {
-  type = string
-  default = "redis-queue"
+variable "ingress_class" {
+  type    = string
+  default = "nginx"
 }
-
 
 ###
 # Redis variables
 ###
+
+variable "redis_release_name" {
+  type    = string
+  default = "redis-queue"
+}
 
 variable "redis_replicaCount" {
   type    = number
@@ -60,4 +64,86 @@ variable "redis_long_name" {
 variable "redis_port" {
   type    = string
   default = "6379"
+}
+
+###
+# Zipkin variables
+###
+
+variable "zipkin_release_name" {
+  type    = string
+  default = "redis-queue"
+}
+variable "zipkin_replicaCount" {
+  type    = number
+  default = 1
+}
+
+variable "zipkin_restartPolicy" {
+  type    = string
+  default = "Always"
+}
+
+variable "zipkin_registry" {
+  type    = string
+  default = "openzipkin/zipkin"
+}
+
+variable "zipkin_pull_policy" {
+  type    = string
+  default = "IfNotPresent"
+}
+
+variable "zipkin_short_name" {
+  type    = string
+  default = "zipkin"
+}
+
+variable "zipkin_long_name" {
+  type    = string
+  default = "workshop-zipkin"
+}
+
+variable "zipkin_port" {
+  type    = string
+  default = "9411"
+}
+
+###
+# Log msg proc variables
+###
+
+variable "log_msg_proc_release_name" {
+  type    = string
+  default = "log-message-processor"
+}
+
+variable "log_msg_proc_replicaCount" {
+  type    = number
+  default = 1
+}
+
+variable "log_msg_proc_restartPolicy" {
+  type    = string
+  default = "Always"
+}
+
+variable "log_msg_proc_registry" {
+  type    = string
+  default = "sgilz/col40-log-message-processor"
+}
+
+variable "log_msg_proc_pull_policy" {
+  type    = string
+  default = "IfNotPresent"
+}
+
+variable "log_msg_proc_short_name" {
+  type    = string
+  default = "log-message-processor"
+}
+
+variable "log_msg_proc_long_name" {
+  type    = string
+  default = "workshop-log-message-processor"
 }
